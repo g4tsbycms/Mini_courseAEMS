4f99c924b6d6d29e8b052cae6ccd29d5e71ba578
weber souza || dev-CMS <61756012+g4tsbycms@users.noreply.github.com>
2023-10-27 19:03:38 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ if(btnDelete) {
   const btnArray = Array.from(btnDelete);
   btnArray.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-      if(!confirm('Are you sure you want to delete it?')){
+      if(!confirm('Tem certeza de que deseja excluir este contato?')){
         e.preventDefault();
       }
     });
